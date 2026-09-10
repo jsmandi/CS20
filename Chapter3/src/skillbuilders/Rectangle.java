97458b5ce8898e25b99c954e713808162324f782
510267017 <510267017@L815WD220017.edu.cbe.ab.ca>
2026-09-10 11:23:11 -0600

    11:22 AM

---
+++ Chapter3/src/skillbuilders/Rectangle.java
@@ -0,0 +1,25 @@
+package skillbuilders;
+
+import java.util.Scanner;
+
+public class Rectangle {
+
+	public static void main(String[] args) 
+	{
+		//Declaration 
+				int length;
+				int width;
+				
+				//Create Scanner object 
+				Scanner userinput = new Scanner(System.in);
+				
+				// Get user input from keyboard
+				System.out.print("Enter width: ");
+				width = userinput.nextInt();
+				
+				// 
+				System.out.print("Enter length: ");
+				length = userinput.nextInt();
+	}
+
+}
