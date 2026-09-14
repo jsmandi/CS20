4cd51c2db6638116601dd8db1509dc260fb7c008
510267017 <510267017@L815WD220017.edu.cbe.ab.ca>
2026-09-14 09:40:22 -0600

    9:40 AM

--- Chapter3/src/skillbuilders/Rectangle.java
+++ Chapter3/src/skillbuilders/Rectangle.java
@@ -6,20 +6,26 @@ public class Rectangle {
 
 	public static void main(String[] args) 
 	{
-		//Declaration 
+		        //Declaration 
 				int length;
 				int width;
+				int area;
 				
 				//Create Scanner object 
 				Scanner userinput = new Scanner(System.in);
 				
-				// Get user input from keyboard
+				// Get user input from keyboard (Width first)
 				System.out.print("Enter width: ");
 				width = userinput.nextInt();
 				
-				// 
+				// Get user input from keyboard (then length)
 				System.out.print("Enter length: ");
 				length = userinput.nextInt();
+				
+				// specify formula for area
+				area = (2 * width) + (2 * length);
+				// Print Perimeter of Rectangle
+				System.out.println("Perimeter of the Rectangle is: " + area);
 	}
 
 }
